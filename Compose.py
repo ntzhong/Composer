1ac5a5979ffe14a2c39b638f531a8ed76bc83ed8
Norman <thenormanzhong@gmail.com>
2016-04-05 19:21:14 -0700

    working on rhythm construction

--- Compose.py
+++ Compose.py
@@ -3,15 +3,15 @@ from midiutil.MidiFile import MIDIFile
 from random import randint
 
 #VALUES TO ADJUST
-COMPOSE_SEPARATELY = True #chooses to construct parts indepndently
+COMPOSE_SEPARATELY = False #chooses to construct parts indepndently
 BPM = 90 #BPM
 CHORDS_PER_MEASURE = 1 #standard is 1 or 2
 PHRASE_LENGTH = 4 #in measures
 SONG_LENGTH = PHRASE_LENGTH * 4 + 1 #in measures. +1 for resolution measure?
 ROOT = 60 #Set tonic to middle C
-TIME_SIGNATURE = 4 #no compound meter. out of 4
+TIME_SIGNATURE = 4 #no compound meter. in beats per measure
 MAJORKEY = True #false => minor
-NUM_TRACKS = 2 #number of tracks
+NUM_TRACKS = 2 #number of tracks. only 1 or 2 for now.
 
 #set range of notes
 minRange = 21 #A0 = 21, lowest on piano
@@ -50,12 +50,18 @@ majSev = 11 #+ octInc
 
 
 time = 0 #in beats. Location in song. must increment
- 
-
 
 #scale construction
 majScale = [tonic, second, majThird, fourth, fifth, majSixth, majSev, tonic+OCTAVE_SIZE]
 minScale = [tonic, second, minThird, fourth, fifth, minSixth, minSev, tonic+OCTAVE_SIZE]
+
+#establish rhythms. 1 = 1 beat
+durations = [0.25, 0.5, 0.75, 1, 1.5, 2, 3, 3.5, 4]
+
+
+
+
+
 
 
 #takes in a MIDI value. returns 3-elem array of midi values
@@ -97,6 +103,47 @@ def inv(triad, n):
 		triad.append(root)
 	return root
 
+
+#return true if curBeat lies within beat. e.g. if 2.5 beats left, then in 2nd beat. beat is a hole number. curBeat is float.
+def withinBeat(curBeat, beat):
+	return (curBeat >= beat) and (curBeat < beat+1)
+
+
+#0 = normal
+#1 = lengthy and emotional
+#2 = short, upbeat, fast, happy
+
+#determines the rhythm for a measure (or phrase?) maybe an array of measure arrays?
+#in comments, swing refers to .75, .25 beats in conjunction
+def determineMelodicRhythm(quality):
+
+	rhythm = []
+	for phrase in range(0, phrases_in_song):
+		for measureNum in range(0, PHRASE_LENGTH):
+				measure = []
+				curBeat = 0
+				duration = 0
+				#an arbitrarily chosen standard style
+				if quality == 0:
+					while curBeat < TIME_SIGNATURE:
+						#determine rhythm of the measure with probability. Depends on location in phrase, possibly also on value of beatsLeft
+						if (measureNum = PHRASE_LENGTH-1): #last measure. 
+							#1st beat: extremely high chance of whole note. Chances: whole note > eigth > quarter > half > swing.
+							if (curBeat == 0):
+
+							#if 2nd beat: eigth >= quarter >= 
+							elif withinBeat(curBeat, 2):
+
+							#else high chance of half note on 3rd beat
+							#...
+							else:
+
+
+
+
+						#duration = duration/CHORDS_PER_MEASURE
+
+
 #returns a chord progression for entirety of song. key_quality = "maj" or "min"
 def constructChordProg(key_quality):
 	#every chord gets equal amount of time, based on num chords per measure
@@ -116,15 +163,21 @@ def constructChordProg(key_quality):
 
 				randVal = randint(0, 20) #determines buckets for probabilities
 				
-				if (measure == 0) && (phrase > 0): #start of phrase, but not song. small chance of relative minor
-					#90% tonic, 10% relative minor
+				if (phrase > 0) and (measure == 0) and (chord == 0): #start of phrase, but not song. small chance of relative minor
+					#80% tonic, 20% relative minor
+					if randVal < 16:
+						chordProgression.append(scale[0])
+					else:
+						chordProgression.append(scale[5])
 
-				elif (measure == 0): #beginning of phrase:
+				elif (phrase==0) and (measure == 0) and (chord == 0): #beginning of song
 					chordProgression.append(scale[0]) #tonic, 100%
 
-				elif (measure == 3) && (phrase == phrases_in_song-1): #end of song
+				elif (phrase == phrases_in_song-1) and (measure == PHRASE_LENGTH-1) and (chord == CHORDS_PER_MEASURE-1): #end of song
 					#dominant = 100%
-				elif (measure == 3): #end of phrase. high chance of dominant. can also be 4
+					chordProgression.append(scale[4])
+
+				elif (measure == PHRASE_LENGTH-1): #end of phrase. high chance of dominant. can also be 4
 					#dominant 85%
 					#subDom = 10%
 
@@ -138,6 +191,10 @@ def constructChordProg(key_quality):
 def compose(file):
 	vol = N
 	harmVol = vol-5
+	if MAJORKEY:
+		chordProg = constructChordProg("maj")
+	else:
+		chordProg = constructChordProg("min")
 
 
 #takes in a MIDIFile object and constructs composition
